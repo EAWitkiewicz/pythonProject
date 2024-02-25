c4a96af84f3dfe0e3730ffe275a21d0182191503
EAWitkiewicz <161207694+EAWitkiewicz@users.noreply.github.com>
2024-02-25 13:20:30 +0100

    nowy plik

--- cwiczenia_1_250224/main.py
+++ cwiczenia_1_250224/main.py
@@ -5,6 +5,7 @@ print(sys.version)
 #git config --list
 # config --global user.name "EAWitkiewicz"
 #git config --global user.email "161207694+EAWitkiewicz@users.noreply.github.com"
-
+#new directory i przesuniecie pliku do folderu comit i push
+#jak chce otworzyc projekt to musze wybrac folder i sciezke z nazwa projektu
 a='zad1'
 print(a)
