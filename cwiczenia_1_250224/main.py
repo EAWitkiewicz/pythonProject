22baa5c6ce3ec4c481bfc780776364651fe19d0f
EAWitkiewicz <161207694+EAWitkiewicz@users.noreply.github.com>
2024-03-03 10:08:05 +0100

    poprawka przedrostka git

--- cwiczenia_1_250224/main.py
+++ cwiczenia_1_250224/main.py
@@ -3,7 +3,7 @@ import sys
 print(sys.version)
 
 #git config --list
-# config --global user.name "EAWitkiewicz"
+#git config --global user.name "EAWitkiewicz"
 #git config --global user.email "161207694+EAWitkiewicz@users.noreply.github.com"
 #new directory i przesuniecie pliku do folderu comit i push
 #jak chce otworzyc projekt to musze wybrac folder i sciezke z nazwa projektu
