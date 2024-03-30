66be11a7709320d93718d3702bd60fb040128959
EAWitkiewicz <161207694+EAWitkiewicz@users.noreply.github.com>
2024-03-30 15:42:52 +0100

    zadanie 1 i 2 z przykladowych do kolosa

--- cwiczenia_1_250224/main.py
+++ cwiczenia_1_250224/main.py
@@ -7,5 +7,4 @@ print(sys.version)
 #git config --global user.email "161207694+EAWitkiewicz@users.noreply.github.com"
 #new directory i przesuniecie pliku do folderu comit i push
 #jak chce otworzyc projekt to musze wybrac folder i sciezke z nazwa projektu
-a='zad1'
 print(a)
---
+++ zadania_do_kolokwium/zadania_na_kolokwium.py
@@ -0,0 +1,36 @@
+import sys
+import math as m
+
+# #zad 1 liczenie i wyswietlanie wyrazen
+# print("Zadanie 1")
+# dzialnie1=(m.exp(4)-m.log2(34))**(1/3)
+# print(dzialnie1)
+# dzialnie2=(m.cos(45)+m.exp(1)+m.log1p(20))**(1/3)
+# print(dzialnie2)
+# dzialnie3=(m.log(20,3)+m.sin(45)*(5/8))**(1/4)
+# print(dzialnie3)
+# dzialnie4=m.log(23,3)+(m.sin(34)+5)**(1/3)
+# print(dzialnie4)
+# dzialnie5=((m.log2(32)+m.pi+m.sin(56)))**(1/4)
+# print(dzialnie5)
+
+# print("\nZadnie 2")
+# podana=input("Podaj wysokosc wierzy miedzy 0 a 10: ")
+# if(int(podana)<=10):
+#     for i in range(int(podana)+1):
+#         for j in range(i):
+#                 print("A",end='')
+#         print()
+# else:print("WIEKSZA OD 10!!!")
+
+print("\nZadnie 3")
+podana1=input("Podaj wysokosc wierzy miedzy 0 a 10: ")
+if(int(podana1)<=10):
+    for i in range(int(podana1)):
+        for k in range(int(podana1)-i):
+            for j in range(k):
+                    print("A",end='')
+        print("_", end='')
+        print()
+else:print("WIEKSZA OD 10!!!")
+#help(m)
