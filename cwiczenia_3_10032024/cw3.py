8d8e0ede6cf91c9be9935869022edd09f19a646e
EAWitkiewicz <161207694+EAWitkiewicz@users.noreply.github.com>
2024-05-21 23:51:21 +0200

    numpy 3/9

--- cwiczenia_3_10032024/cw3.py
+++ cwiczenia_3_10032024/cw3.py
@@ -85,13 +85,10 @@ import math
 #     return math.sqrt((x1-x2)**2 + (y1-y2)**2)
 #
 #
-# print(dlugosc_odcinka())
-#
-# print(dlugosc_odcinka(4,2,6,3))
-#
-# print(dlugosc_odcinka(y2=5,y1=5,x2=2,x1=8))
-#
-# print(dlugosc_odcinka(3,5,y2=4,y1=3))
+# print(dlugosc_odcinka())                                #wyswietlenie funkcji
+# print(dlugosc_odcinka(4, 2, 6, 3))       #podajemy w kolejnsci
+# print(dlugosc_odcinka(y2 = 3, x2 = 2, y1 = 3, x1 = 6))  #poza kolejnoscia_1
+# print(dlugosc_odcinka(3, 5, y2 = 4, y1 = 3))     #poza kolejnoscia_2
 
 
 ### operacje na plikach
--- cwiczenia_3_10032024/cw31.py
+++ cwiczenia_3_10032024/cw31.py
@@ -22,7 +22,38 @@
 # plik.close()
 # #plik=
 
+# #przeniesc plik na nazwe projektu
+# plik = open('tekst.txt', 'r', encoding='utf-8') #encoding - polskie znaki, r - czytanie / w - plik nie musi istniec
+# znaki = plik.read(10) #wczyta 10 znakow
+#
+# linia = plik.readline() #odczyta cala linijke
+# linie = plik.readlines() #odczyta cala zawartosc
+#
+# print(znaki)
+# print(linia)
+# print(linie)
+#
+# for l in linie:
+#     print(l)
+
+# plik = open('tekst1.txt', 'w', encoding='utf-8')    #doda plik  tekstowy z zawartoscia aaaaaaaaaaaaaaaaaaaa, jesli podalibysmy istniejacy plik t nadpiszemy zawartosc, a+ zamiast w mozena odczytywac i nadpisywac zawartosc
+# plik.write('aaaaaaaaaaaa')
+# plik.close()
+
+
+# plik = open('tekst.txt', 'a+')      #w+ zamiast a+ czysci cala zawartosc
+# plik.write('aaaaaaaaaaaaaaassscxcxz')
+# plik.seek(105)
+# znaki = plik.read(10)
+# plik.close()
+# print(znaki)
+
+# with open('tekst.txt', 'r') as f:      #odczytanie zawartosci, roznica jest taka ze nie trzeba zamykac pliku, z automatu sie zamknie
+#     lines = f.readlines()
+# print(lines)
+
+
 #nie trzeba pamietac o zamykaniu pliku
-with open('tekst.txt', 'r') as f:
-    lines = f.readlines()
-print(lines)
+# with open('tekst.txt', 'r') as f:
+#     lines = f.readlines()
+# print(lines)
