171c009af8a878ee8050ccd1a6167ca172db8ab7
EAWitkiewicz <161207694+EAWitkiewicz@users.noreply.github.com>
2024-06-04 12:10:49 +0200

    nupy zakomentowany

--- cwiczenia_5_26052024/lab_numpy.py
+++ cwiczenia_5_26052024/lab_numpy.py
@@ -167,31 +167,3 @@ import numpy as np
 #  # [2 5]]
 # print(e.shape)
 
-
-# CO DO NUMPY TO TYLE
-# import pandas as pd
-#
-# s = pd.Series([1, 3, 5, np.nan, 6, 8])
-# print(s)
-# s = pd.Series([10, 12, 8, 14], index=['a', 'b', 'c', 'd'])
-# print(s)
-# data = {'Kraj': ['Belhia', 'Indie', 'Brazylia'],
-#         'Stolica': ['Bruksela', 'New Delhi', 'Brazylia'],
-#         'Populacja': [11190846, 13031171035, 207847528]}
-# df = pd.DataFrame(data)
-# print(df)
-# print(df.dtypes)
-# df = pd.read_csv('dane.csv', header=0, sep=";", decimal=',')
-# print(df)
-# df.to_csv('plik.csv', index=False)
-#
-# print(s['c'])
-# print(s.c)
-# print(df[0:1])
-# print("")
-# print(df['Populacja'])
-# print(df.iloc[0.0])
-# print(df.loc[0, "Kraj"])
-# print(df.a[0, "Kraj"])
-# print('kraj' + df.Kraj)
-# print(df.sample())
