8d134aeca958d3f756acf6af64374c1add333699
EAWitkiewicz <161207694+EAWitkiewicz@users.noreply.github.com>
2024-06-06 13:04:06 +0200

    zad 1 i 2 z przykladowego kolowkium

--- zadania_do_kolokwium/kolokwium 2.py
+++ zadania_do_kolokwium/kolokwium 2.py
@@ -15,17 +15,41 @@ print(os_x)
 
 #Zad 2. (8pkt.) Za pomocą biblioteki pomocą pandas wczytaj zawartość pliku
 # „automobile.csv” do ramki danych, a następnie:
+dane=pd.read_csv('automobile.csv',sep=';',decimal='.',header=0)
+
 #Utwórz i wyświetl nową ramkę danych składającą się z rekordów dla
 # samochodów audi i dodge.
-
+audi_doge=dane[(dane['Car model']=='audi') | (dane['Car model']=='dodge')]
+print(audi_doge)
 #Na nowej ramce danych dokonaj grupowania danych po kolumnie „Car model”
 # a następnie policz sumę wartości dla tych samochodów (kolumna Price)
-
+modele=dane.groupby(['Car model'])
+suma_cen=modele['Price'].sum()
+print(suma_cen)
 #Na podstawie utworzonej grupy utwórz wykres słupkowy, dodaj tytuł oraz
 # etykiety dla osi x i y. Dopasuj rozmiar wykresu tak aby był on widoczny w całości.
-
+auta=np.array(list(modele.groups.keys()))
+ceny=list(suma_cen)
+print(ceny)
+fig,ax=plt.subplots()
+color=plt.cm.viridis(np.linspace(0,1),len(auta))
+ax.bar(x=auta,height=ceny,color=color)
+plt.title('Ceny aut')
+plt.xlabel('Modele aut')
+plt.ylabel('Cena ')
+plt.xticks(rotation=45,ha='right')
+plt.subplots_adjust(bottom=0.3,left=0.15)
+#plt.legend()
+plt.show()
 #Zad 3. (6pkt) Za pomocą biblioteki pomocą pandas wczytaj zawartość pliku
 # „automobile.csv” do ramki danych i  utwórz wykres kołowy przedstawiający
 # procentową ilość samochodów z danym rodzajem paliwa (kolumna Fuel-type).
 # Procentowe wartości mają być zaokrąglone do dwóch miejsc po przecinku,
 # rozmiar czcionki 14. Dodaj tytuł i legendę.
+
+# Policz ilość samochodów dla każdego rodzaju paliwa
+ile=dane['Fuel type'].value_counts()
+
+fig,ax=plt.subplots()
+ax.pie(ile,labels=ile.index,autopct='%2.f',fontsize=14)
+plt.show()
